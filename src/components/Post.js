402d57f9c230540055749aaafbea82a058c6fb50
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-09-23 18:43:36 +0200

    creation article

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Like from "./Like";
+import { isEmpty } from "../components/Utils";
 
 const Post = ({ post }) => {
+  const user = useSelector((state) => state.userReducer)
+
   return (
     <div className="post">
+      {!isEmpty(user[0]) && user[0].pseudo === post.author && (
+        <div className="edit-delete">
+          <img src="../icons/edit.svg" alt="edit" />
+          <img src="../icons/delete" alt="delete" />
+        </div>
+      )
+      }
+
       <h2>{post.title}</h2>
       <img
         src="https://picsum.photos/1500/400"
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost, getPosts } from "../actions/post.action";
 
 const PostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const user = useSelector((state) => state.userReducer)
+  const dispatch = useDispatch()
+
+  const handleForm = async (e) => {
+    e.preventDefault()
+
+    if (title && content) {
+      const data = {
+        title,
+        content,
+        author: user[0].pseudo,
+        likes: 0,
+      }
+      await dispatch(addPost(data))
+      setTitle('')
+      setContent('')
+      dispatch(getPosts())
+    }
+  }
 
   return (
     <div className="form-container">
-      <form>
+      <form onSubmit={(e) => handleForm(e)}>
         <input
           type="text"
           placeholder="Titre du poste"
